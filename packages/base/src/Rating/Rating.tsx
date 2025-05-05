cb75d8c430b5f76f430d4d91924994a54a95d836
Deepak Yadav <76097094+deepktp@users.noreply.github.com>
2025-05-05 18:10:10 +0530

    feat: update ratings dependency to @rn-vui/ratings and remove react-native-ratings issue#25

--- packages/base/src/Rating/Rating.tsx
+++ packages/base/src/Rating/Rating.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {
   Rating as SwipeRating,
   SwipeRatingProps as RatingProps,
-} from 'react-native-ratings';
+} from '@rn-vui/ratings';
 import { RneFunctionComponent } from '../helpers';
 
-export interface SwipeRatingProps extends RatingProps {}
+export interface SwipeRatingProps extends RatingProps { }
 
 /** Ratings are used to collect measurable feedback from users.
  * Use Rating over an Input where imagery can increase user interaction.
- * This component is imported from [react-native-ratings](https://github.com/Monte9/react-native-ratings).
+ * This component is imported from [@rn-vui/ratings](https://github.com/deepktp/react-native-vikalp-ratings).
  * There are two tyoes of rating - TapRating and SwipeRating.
  * This documentation is for Swipe Rating version. */
 export const Rating: RneFunctionComponent<SwipeRatingProps> = (props) => {
