7117208dfca60a5ab50655173c4d2943c0d009c1
React Native vElements CI <react-native-elements-ci@users.noreply.github.com>
2025-07-11 06:11:25 +0000

    Prepare Bleeding Edge Version

---
+++ dist/AirbnbRating/AirbnbRating.d.ts
@@ -0,0 +1,5 @@
+import { TapRatingProps as RatingProps } from '@rn-vui/ratings';
+import { RneFunctionComponent } from '../helpers';
+export interface TapRatingProps extends RatingProps {
+}
+export declare const AirbnbRating: RneFunctionComponent<TapRatingProps>;
---
+++ dist/Rating/Rating.d.ts
@@ -0,0 +1,5 @@
+import { SwipeRatingProps as RatingProps } from '@rn-vui/ratings';
+import { RneFunctionComponent } from '../helpers';
+export interface SwipeRatingProps extends RatingProps {
+}
+export declare const Rating: RneFunctionComponent<SwipeRatingProps>;
